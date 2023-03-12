0eeb0560dc1b8558dc2f42e97371678fcc48ef4c
HripyVoskanyan <75030550+HripyVoskanyan@users.noreply.github.com>
2023-03-12 15:49:17 +0400

    Add files via upload

---
+++ assets/config1.py
@@ -0,0 +1,18 @@
+main_tag = 'item'
+keyword = "bedroom"
+mainpage = 'https://myrealty.am/en/apartments-for-sale/Yerevan/7762'
+pagenum = 3
+sign = '?'
+class_ = 'image'
+
+config = {'main': ['div', 'container'],
+          'address': ['div', 'col-auto item-view-address d-none d-xl-block mr-0'],
+          'sqm': ['div', 'col-12 d-flex justify-content-between justify-content-sm-start item-view-price-params'],
+          'info': ['h1', 'item-view-title'],
+          'price': ['div', 'item-view-price'],
+          'more': ['ul', 'item-view-list-params'],
+          'facilities': ['div', 'row item-view-facilities mb-4', 'li'],
+          'additional': ['div', 'row item-view-facilities mb-4', 'li'],
+          'views': ['span', 'item-view-count'],
+          'coord': ['div', 'row no-gutters mb-4']
+          }
---
+++ assets/config2.py
@@ -0,0 +1,19 @@
+main_tag2 = 'card mb-3 p-2 rounded-0 estate-container'
+keyword2 = "apartment"
+mainpage2 = 'https://besthouse.am/en/search?currency_id=USD&estate_region=1&estate_role_id=1&estate_type_id=1'
+pagenum2 = 3
+sign2 = '&'
+class_2 = 'd-block'
+
+config2 = {'main': ['div', 'estate-container col-md-12'],
+          'address': ['div', 'estate__address text-dark py-1'],
+          'sqm': ['li', 'd-flex align-items-center py-1', 1],
+          'rooms': ['li', 'd-flex align-items-center py-1', 0],
+          'floor': ['li', 'd-flex align-items-center py-1', 2],
+          'price': ['div', 'estate__price d-flex justify-content-start align-items-center lead font-weight-bold'],
+          'more': ['ul', 'item-view-list-params'],
+          'facilities': ['div', 'estate__facilities text-secondary text-uppercase py-3', 'li'],
+          'additional': ['div', 'estate__facilities text-secondary text-uppercase py-3 col-md-8', 'li'],
+          'views': ['span', 'item-view-count'],
+          'coord': ['div', 'row no-gutters mb-4']
+          }
