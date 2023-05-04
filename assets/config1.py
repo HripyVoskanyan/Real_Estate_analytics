281fa59e7a2e89ffc6e8032ceabb9ecf1ed58604
HripyVoskanyan <75030550+HripyVoskanyan@users.noreply.github.com>
2023-05-04 21:54:48 +0400

    Add files via upload

--- assets/config1.py
+++ assets/config1.py
@@ -1,18 +1,20 @@
-main_tag = 'item'
-keyword = "bedroom"
-mainpage = 'https://myrealty.am/en/apartments-for-sale/Yerevan/7762'
-pagenum = 3
-sign = '?'
-class_ = 'image'
+sitemap_url = "https://myrealty.am/sitemap.xml"
 
-config = {'main': ['div', 'container'],
-          'address': ['div', 'col-auto item-view-address d-none d-xl-block mr-0'],
-          'sqm': ['div', 'col-12 d-flex justify-content-between justify-content-sm-start item-view-price-params'],
-          'info': ['h1', 'item-view-title'],
-          'price': ['div', 'item-view-price'],
-          'more': ['ul', 'item-view-list-params'],
-          'facilities': ['div', 'row item-view-facilities mb-4', 'li'],
-          'additional': ['div', 'row item-view-facilities mb-4', 'li'],
-          'views': ['span', 'item-view-count'],
-          'coord': ['div', 'row no-gutters mb-4']
-          }
+config = {
+    "main": ["div", "container"],
+    "address": ["div", "col-auto item-view-address d-none d-xl-block mr-0"],
+    "sqm": [
+        "div",
+        "col-12 d-flex justify-content-between justify-content-sm-start item-view-price-params",
+    ],
+    "info": ["h1", "item-view-title"],
+    "price": ["div", "item-view-price"],
+    "more": ["ul", "item-view-list-params"],
+    "facilities": ["div", "row item-view-facilities mb-4", "li"],
+    "additional": ["div", "row item-view-facilities mb-4", "li"],
+    "views": ["span", "item-view-count"],
+    "coord": ["div", "row no-gutters mb-4"],
+    "adddate": ["div", "col-auto mb-1"],
+    "editdate": ["div", "col-auto"],
+    "id": ["div", "item-view-id"],
+}
