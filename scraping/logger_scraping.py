023464d9e79388b4d2e9161d9454375f5f5ad301
HripyVoskanyan <75030550+HripyVoskanyan@users.noreply.github.com>
2023-05-13 18:14:02 +0400

    technical improvements

--- scraping/logger_scraping.py
+++ scraping/logger_scraping.py
@@ -2,9 +2,12 @@ import logging
 from config import log_folder, log_file_scrap
 import os
 
+logfolder = os.getcwd() + '\\' + log_folder
+if not os.path.exists(logfolder):
+    os.makedirs(logfolder)
 
 logging.basicConfig(
     level=logging.INFO,
-    filename=os.path.join(log_folder, log_file_scrap),
+    filename=os.path.join(logfolder, log_file_scrap),
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
