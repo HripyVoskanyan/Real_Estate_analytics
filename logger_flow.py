437f2524be706f3b167f1f9268cb0b418e5885dc
HripyVoskanyan <75030550+HripyVoskanyan@users.noreply.github.com>
2023-05-13 18:15:29 +0400

    technical adjustments

--- logger_flow.py
+++ logger_flow.py
@@ -2,9 +2,13 @@ import logging
 from config import log_folder, log_file_flow
 import os
 
+logfolder = os.getcwd() + '\\' + log_folder
+if not os.path.exists(logfolder):
+    os.makedirs(logfolder)
+
 
 logging.basicConfig(
     level=logging.INFO,
-    filename=os.path.join(log_folder, log_file_flow),
+    filename=os.path.join(logfolder, log_file_flow),
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
--- logger_infrastructure.py
+++ logger_infrastructure.py
@@ -2,9 +2,12 @@ import logging
 from config import log_folder, log_file_infr
 import os
 
+logfolder = os.getcwd() + '\\' + log_folder
+if not os.path.exists(logfolder):
+    os.makedirs(logfolder)
 
 logging.basicConfig(
     level=logging.INFO,
-    filename=os.path.join(log_folder, log_file_infr),
+    filename=os.path.join(logfolder, log_file_infr),
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
